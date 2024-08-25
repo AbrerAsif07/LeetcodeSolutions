011d2660a992a017219580cfe3a04291eaec2d2c
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-08-25 21:31:10 +0530

    hashing using dic

--- Hashing/q1.py
+++ Hashing/q1.py
@@ -1,1 +1,16 @@
 # Hashing deals with the logic of prestoring frequency and then fetching
+lst = [5, 4, 2, 5, 4, 2, 1, 1, 1, 3, 5, 3]
+
+dic = {}
+
+for num in lst:
+    dic[num] = dic.get(num, 0) + 1
+print(dic)
+
+x = int(input("Enter no whose repition value you want to see = "))
+print(dic.get(x, 0))
+
+
+lst = [5, 2, 1]
+for num in lst:
+    print(dic.get(num, 0))
