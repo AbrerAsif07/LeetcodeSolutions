25c33d5dc02cf7e6301de62d27847dfb772b6d89
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-08-28 20:10:11 +0530

    palindrome

---
+++ Basic math questions/palindrome.py
@@ -0,0 +1,15 @@
+def check_palindrome(num: int) -> bool:
+    result = 0
+    n = num
+    while n > 0:
+        ld = n % 10
+        result = (result * 10) + ld
+        n = n // 10
+    return result == num
+
+
+print(check_palindrome(1))
+
+
+TC = O(log10N)
+SC = O(1)
