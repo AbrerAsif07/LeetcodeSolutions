8bdad845af6d640d5c1e0c7f487c1416a817e175
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-09-14 22:12:45 +0530

    14 september

--- 4.Sorting/2.Inserton_sort.py
+++ 4.Sorting/2.Inserton_sort.py
@@ -1,3 +1,16 @@
 # Insertion sort  is a sorting algorithm that uses i,j and key
-print("Hello world")
-print("Hello world")
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = key
+
+
+arr = [3, 5, 6, 86, 28, 7, 12]
+insertion_sort(arr)
+print(arr)
