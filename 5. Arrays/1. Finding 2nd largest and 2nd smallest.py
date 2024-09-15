b1ac411e6b53d76fb49db3069525f3a48ab7719e
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-09-15 13:50:20 +0530

    second largest and smallest elements

---
+++ 5. Arrays/1. Finding 2nd largest and 2nd smallest.py
@@ -0,0 +1,26 @@
+def secondarr(arr):
+    maxi = float("-inf")
+    second_maxi = float("-inf")
+    mini = float("inf")
+    second_mini = float("inf")
+    n = len(arr)
+    for i in range(0, n):
+        maxi = max(maxi, arr[i])
+        mini = min(mini, arr[i])
+
+    for i in range(0, n):
+        if arr[i] > second_maxi and arr[i] != maxi:
+            second_maxi = arr[i]
+
+        if arr[i] < second_mini and arr[i] != mini:
+            second_mini = arr[i]
+
+    return second_maxi, second_mini
+
+
+arr = [5, -10, 13, 67, 89, -100, 55]
+second_largest, second_smallest = secondarr(arr)
+
+# Output the results
+print("Second largest:", second_largest)
+print("Second smallest:", second_smallest)
