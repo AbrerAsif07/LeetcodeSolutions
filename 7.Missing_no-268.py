f601d45af490228582200f4a6c79cd0aa1e4a02b
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-09-17 17:12:18 +0530

    missing no using subtration method

---
+++ 7.Missing_no-268.py
@@ -0,0 +1,9 @@
+def missNo(nums):
+    n = len(nums)
+    ori_arr_total = (n * (n + 1)) // 2
+    return ori_arr_total - sum(nums)
+
+
+nums = [3, 0, 1]
+missing_no = missNo(nums)
+print(f"missing no is = {missing_no}")
