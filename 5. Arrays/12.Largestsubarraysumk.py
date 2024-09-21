3d3c8c2c11eaeedcf6d7ff0f9f9b9097071c7cd9
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-09-21 22:34:07 +0530

    yo

--- 5. Arrays/12.Largestsubarraysumk.py
+++ 5. Arrays/12.Largestsubarraysumk.py
@@ -1,3 +1,6 @@
+# Given an array arr containing n integers and an integer k. 
+# Your task is to find the length of the longest Sub-Array with the sum of the elements equal to the given value k.
+
 def LargestSubarray(arr, k):
     n = len(arr)
     sum_map = {}
@@ -22,3 +25,7 @@ def LargestSubarray(arr, k):
 arr = [1, 2, 3, 1, 1, 1, 1, 3, 3]
 x = LargestSubarray(arr, 3)
 print(x)
+
+
+# TC=O(N)
+# SC=O(1)
