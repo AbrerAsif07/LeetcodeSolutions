c4f9e8a6bb3327ddb7678d1bd93d72b867d99907
AB <157715929+AbrerAsif07@users.noreply.github.com>
2024-10-21 14:20:49 +0530

    peak element

---
+++ 6.Binary Search/11.PeakElement.py
@@ -0,0 +1,17 @@
+# nums=[5,10,20,15]
+def peakElement(nums):  # return type is index
+    l = 0
+    h = len(nums) - 1
+    while l < h:
+        mid = (l + h) // 2
+        if nums[mid] < nums[mid + 1]:
+            l = mid + 1
+        else:
+            h = mid
+
+    return l
+
+
+nums = [5, 4, 3, 2, 1]
+x = peakElement(nums)
+print(x)
